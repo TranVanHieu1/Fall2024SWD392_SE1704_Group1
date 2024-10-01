2f83da819474277d8fba48c2e707bf0f04e03c85
TranVanHieu1 <123tranhieu123@gmail.com>
2024-10-01 14:21:13 +0700

    Cố gắng lên

--- src/main/java/com/group1/Care_Koi_System/config/OpenApi30Config.java
+++ src/main/java/com/group1/Care_Koi_System/config/OpenApi30Config.java
@@ -2,18 +2,44 @@ package com.group1.Care_Koi_System.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+
+import java.util.List;
+
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Care-koi", version = "v1"))
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-)
-public class OpenApi30Config {
 
+public class OpenApi30Config {
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl("https://fall2024swd392-se1704-group1.onrender.com");
+        devServer.setUrl("http://localhost:8080");
+        devServer.setDescription("Server URL in Development environment");
+
+        Info info = new Info()
+                .title("Care-Koi")
+                .version("1.0.0");
+
+        return new OpenAPI().info(info)
+                .servers(List.of(devServer)) // Ensure Java 9+ or use Collections.singletonList(devServer)
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("bearerAuth"))  // Make sure this matches 'bearerAuth'
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", createAPIKeyScheme())); // Make sure this matches 'bearerAuth'
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
 }
