fb9d46babe6707cd65d3a6b4889f72bf6bd5d634
TranVanHieu1 <123tranhieu123@gmail.com>
2024-10-25 08:23:23 +0700

    update create waterparameter

--- src/main/java/com/group1/Care_Koi_System/service/WaterParameterService.java
+++ src/main/java/com/group1/Care_Koi_System/service/WaterParameterService.java
@@ -109,9 +109,7 @@ public class WaterParameterService {
 
 
     public ResponseEntity<ResponseException> updateWaterParameter(int id, WaterParameterRequest waterParameterRequest){
-
         try {
-
             Account account = accountUtils.getCurrentAccount();
             Ponds ponds = pondRepository.findById(id);
             if(account == null) {
@@ -120,74 +118,26 @@ public class WaterParameterService {
             if (ponds == null) {
                 throw new SystemException(ErrorCode.POND_NOT_FOUND);
             }
-
             WaterParameter waterParameter = new WaterParameter();
 
-            try {
-                try{
+                try {
                     waterParameter.setPond(ponds);
                     waterParameter.setCheckDate(LocalDateTime.now());
-                    // Kiểm tra phần trăm muối
-                    double percentSalt = Double.parseDouble(waterParameterRequest.getPercentSalt());
-                    if (percentSalt < 0.1 || percentSalt > 0.5) {
-                        throw new SystemException(ErrorCode.PERCENTSALT);
-                    }
-                    waterParameter.setPercentSalt(waterParameterRequest.getPercentSalt());
 
-                    // Kiểm tra nhiệt độ
-                    double temperature = Double.parseDouble(waterParameterRequest.getTemperature());
-                    if (temperature < 15.0 || temperature > 30.0) { // Điều chỉnh theo khoảng nhiệt độ phù hợp
-                        throw new SystemException(ErrorCode.TEMPERATURE);
-                    }
-                    waterParameter.setTemperature(waterParameterRequest.getTemperature());
-
-                    // Kiểm tra NO₂
-                    double no2 = Double.parseDouble(waterParameterRequest.getNO2());
-                    if (no2 > 0.20) {
-                        throw new SystemException(ErrorCode.NO2);
-                    }
-                    waterParameter.setNO2(waterParameterRequest.getNO2());
-
-                    // Kiểm tra NO₃
-                    double no3 = Double.parseDouble(waterParameterRequest.getNO3());
-                    if (no3 < 10 || no3 > 20) {
-                        throw new SystemException(ErrorCode.NO3);
-                    }
-                    waterParameter.setNO3(waterParameterRequest.getNO3());
-
-                    // Kiểm tra O₂
-                    double o2 = Double.parseDouble(waterParameterRequest.getO2());
-                    if (o2 < 6.0 || o2 > 8.0) { // Điều chỉnh theo khoảng O₂ phù hợp
-                        throw new SystemException(ErrorCode.O2);
-                    }
-                    waterParameter.setO2(waterParameterRequest.getO2());
-
-                    // Kiểm tra pH
-                    double pH = Double.parseDouble(waterParameterRequest.getPH());
-                    if (pH < 7.0 || pH > 8.5) {
-                        throw new SystemException(ErrorCode.PH);
-                    }
-                    waterParameter.setPH(waterParameterRequest.getPH());
-
-
-                }catch (SystemException ex){
-                    ErrorCode errorCode = ex.getErrorCode();
-                    ResponseException response = new ResponseException(ex.getMessage());
-                    return new ResponseEntity<>(response, errorCode.getHttpStatus());
+                    validateAndSetWaterParameters(waterParameter, waterParameterRequest);
+                    waterParameterRepository.save(waterParameter);
+                    ResponseException response = new ResponseException("Update successful!");
+                    return new ResponseEntity<>(response, HttpStatus.OK);
+                } catch (SystemException ex) {
+                    throw new SystemException(ErrorCode.CAN_NOT_SAVE);
                 }
-
-                waterParameterRepository.save(waterParameter);
-                ResponseException response = new ResponseException("Update successful!");
-                return new ResponseEntity<>(response, HttpStatus.OK);
-            } catch (SystemException ex) {
-                throw new SystemException(ErrorCode.CAN_NOT_SAVE);
+            }catch (SystemException ex) {
+                ErrorCode errorCode = ex.getErrorCode();
+                ResponseException response = new ResponseException(ex.getMessage());
+                return new ResponseEntity<>(response, errorCode.getHttpStatus());
             }
-        }catch (SystemException ex){
-            ErrorCode errorCode = ex.getErrorCode();
-            ResponseException response = new ResponseException(ex.getMessage());
-            return new ResponseEntity<>(response, errorCode.getHttpStatus());
-        }
     }
+
 
     public ResponseEntity<WaterParameterResponse> getWaterParameterByPond(int id){
         try{
